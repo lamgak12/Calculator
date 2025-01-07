b31c422a0f382e252850ccecfb1f3e0d92527e07
lamgak12 <tntnqk12@naver.com>
2025-01-07 15:40:50 +0900

    chore(calculator2): 계산기 Lv2 뼈대 추가

---
+++ src/main/java/com/example/calculator2/Main.java
@@ -0,0 +1,8 @@
+package com.example.calculator2;
+
+public class Main {
+    public static void main(String[] args) {
+        Run app = new Run();
+        app.start();
+    }
+}
---
+++ src/main/java/com/example/calculator2/Run.java
@@ -0,0 +1,12 @@
+package com.example.calculator2;
+
+public class Run {
+    public void start() {
+        InputNumber inputNumber = new InputNumber();
+        InputOperator inputOperator = new InputOperator();
+        Calculator calculator = new Calculator();
+        ResultPrinter resultPrinter = new ResultPrinter();
+        ResultStroage resultStroage = new ResultStroage();
+
+    }
+}
