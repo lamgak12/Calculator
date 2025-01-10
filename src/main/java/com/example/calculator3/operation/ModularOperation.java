cdc52322846c93134a50ffb07db2a6021a7048da
lamgak12 <tntnqk12@naver.com>
2025-01-10 22:26:30 +0900

    feat(ModularOperation): 예외 검증 로직 추가

--- src/main/java/com/example/calculator3/operation/ModularOperation.java
+++ src/main/java/com/example/calculator3/operation/ModularOperation.java
@@ -1,10 +1,20 @@
 package com.example.calculator3.operation;
 
+import static java.lang.Double.NaN;
+
 public class ModularOperation<T extends Number> implements Operation<T> {
     @Override
     public T operate(T firstNumber, T secondNumber) {
+        // secondNumber를 Double로 변환하여 0인지 확인
         if (secondNumber.doubleValue() == 0) {
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+            // secondNumber가 Double일 경우 NaN 반환
+            if (secondNumber instanceof Double) {
+                return (T) Double.valueOf(NaN); // Double.NaN 반환
+            } else if(secondNumber instanceof Float) {
+                return (T) Float.valueOf(Float.NaN);
+            }else {
+                return (T) Double.valueOf(NaN);
+            }
         }
         if (firstNumber instanceof Double || secondNumber instanceof Double) {
             // 하나라도 Double이면 결과는 Double
